5959f1f44cc162fb50e653bd21f2cda69031d35d
AlexNalotov <80322829+AlexNalotov@users.noreply.github.com>
2023-06-19 17:45:01 +0300

    Update tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+function litres(time) {
+    let getWatter= Math.floor(time * 0.5);
+    return getWatter ;
+  }
+
+
+  // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+  function litres(time) {
+    let getWatter= Math.floor(time * 0.5);
+    return getWatter ;
+  }
