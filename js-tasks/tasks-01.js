c231ee50faf7794a4c29904dc34e3e2696b3a78d
LukashevychV <128608593+LukashevychV@users.noreply.github.com>
2023-06-21 16:52:33 +0300

    Update tasks-01.js
    Part of JS tasks 2-4 + 1 additional

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -12,3 +12,48 @@ function litres(time) {
     return words;
 }
 
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+function DNAtoRNA(dna) {
+  return dna.replaceAll("T", "U");
+}
+
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+var min = function (list) {
+  list.sort((a, b) => a - b);
+  return list[0];
+};
+
+var max = function (list) {
+  list.sort((a, b) => b - a);
+  return list[0];
+};
+
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+
+function min(arr, toReturn) {
+  switch (toReturn) {
+    case "value":
+      return Math.min(...arr);
+    case "index":
+      return arr.indexOf(Math.min(...arr));
+  }
+}
+
+//https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+
+function warnTheSheep(queue) {
+  queue.reverse();
+  var result;
+  for (var i = 0; i <= queue.length - 1; i++) {
+    console.log(i);
+    if (queue[i + 1] === "wolf") {
+      return `Oi! Sheep number ${i + 1}! You are about to be eaten by a wolf!`;
+    }
+    if (queue[i] === "wolf") {
+      return "Pls go away and stop eating my sheep";
+    }
+  }
+}
