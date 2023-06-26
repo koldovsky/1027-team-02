671059dd347ae0e88d2ef5224e964799cc800f8a
LukashevychV <128608593+LukashevychV@users.noreply.github.com>
2023-06-26 14:36:05 +0000

    fix some not pretty thinks in Process

--- js/process.js
+++ js/process.js
@@ -2,8 +2,8 @@ const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
-const countdown = document.getElementById('countdown');
-const year = document.getElementById('year');
+const new_year__countdown = document.getElementById('countdown');
+const new_year__year = document.getElementById('year');
 const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
@@ -11,7 +11,7 @@ const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
 // Set background year
-year.innerText = currentYear + 1;
+new_year__year.innerText = currentYear + 1;
 
 // Update countdown time
 function updateCountdown() {
@@ -33,7 +33,7 @@ function updateCountdown() {
 // Show spinner before countdown
 setTimeout(() => {
   loading.remove();
-  countdown.style.display = 'flex';
+  new_year__countdown.style.display = 'flex';
 }, 1000);
 
 // Run every second
