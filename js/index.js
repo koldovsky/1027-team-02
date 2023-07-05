b96daf479313ee92648c15217f09949fd0a398d7
RomanButts <138126102+RomanButts@users.noreply.github.com>
2023-07-05 09:39:48 +0000

    JSreadmore

--- js/index.js
+++ js/index.js
@@ -2,8 +2,9 @@ document.addEventListener('partialsLoaded', () => {
     import('./industries.js');
     import('./accordion.js');
     import('./clients.js');
-    import('./weexplore.js');
     import('./process.js');
+    import('./weexplore.js');
+
 });
 
 
