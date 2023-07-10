349fd0d052b16169a2adfe9832b6aab008916c3a
LukashevychV <128608593+LukashevychV@users.noreply.github.com>
2023-07-10 01:48:41 +0000

    Process changes add clock

--- js/process.js
+++ js/process.js
@@ -1,40 +1,18 @@
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
-const new_year__countdown = document.getElementById('countdown');
-const new_year__year = document.getElementById('year');
-const loading = document.getElementById('loading');
+function updateClock() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
 
-const currentYear = new Date().getFullYear();
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
 
-const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+    let amPm = (hours < 12 ? "AM" : "PM");
+    hours = (hours > 12 ? hours - 12 : hours);
+    hours = (hours === 0 ? 12 : hours);
 
-// Set background year
-new_year__year.innerText = currentYear + 1;
+    let clockElement = document.getElementById("clock");
+    clockElement.innerHTML = hours + ":" + minutes + ":" + seconds + " " + amPm;
+  }
 
-// Update countdown time
-function updateCountdown() {
-  const currentTime = new Date();
-  const diff = newYearTime - currentTime;
-
-  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-  const m = Math.floor(diff / 1000 / 60) % 60;
-  const s = Math.floor(diff / 1000) % 60;
-
-  // Add values to DOM
-  days.innerHTML = d;
-  hours.innerHTML = h < 10 ? '0' + h : h;
-  minutes.innerHTML = m < 10 ? '0' + m : m;
-  seconds.innerHTML = s < 10 ? '0' + s : s;
-}
-
-// Show spinner before countdown
-setTimeout(() => {
-  loading.remove();
-  new_year__countdown.style.display = 'flex';
-}, 1000);
-
-// Run every second
-setInterval(updateCountdown, 1000);
+  setInterval(updateClock, 1000);
